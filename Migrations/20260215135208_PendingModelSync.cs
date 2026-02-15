7d85c630a5ca13d778c87f5d1190b598d4e8ad4c
Gustavo Larco <gustavo.larcoj@gmail.com>
2026-02-15 07:52:39 -0600

    migration: sync pending model changes

---
+++ Migrations/20260215135208_PendingModelSync.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gimnasio.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingModelSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Productos",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Productos");
+        }
+    }
+}
